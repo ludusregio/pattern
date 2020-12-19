da308c8daaedecad30ba47436b851d914f544bd2
Fernando Jimenez <20340262+ludusregio@users.noreply.github.com>
2020-12-19 15:00:07 -0700

    Adds start.py

---
+++ hide-list/start.py
@@ -0,0 +1,22 @@
+creatures = [
+        "Pinguin",
+        "Seal",
+        "Moose",
+        "Dog",
+        "Elephant",
+        "Sloth",
+        "Gorilla",
+        "Giraffe",
+        "Polar Bear",
+        "Platypus",
+        "Orca",
+        "Whale",
+        "Squirel",
+        "Mouse",
+        "Meerkat",
+        "Flamingo"
+        ]
+
+filename = "./natural_world.txt"
+
+# You should not need to modify the code above
